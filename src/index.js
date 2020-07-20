063631ff9047267d59500d29e12790511e743386
Vladislav Kostin <v1adyank0stin@gmail.com>
2020-07-20 18:12:32 +0400

    func-const

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import {Character} from './js/app';
-
-// TODO: write your code in app.js
-
-let one = new Character('One', "Undead");
--- src/js/app.js
+++ src/js/app.js
@@ -1,41 +1,5 @@
-export default function Character(name, type) {
-  if((typeof name === "String" && (name.length > 2 && name.length < 10)) &&
-  ["Bowman", "Swordsman", "Magician", "Daemon", "Undead", "Zombie"].includes(type)){
-    this.name = name;
-    this.type = type;
-    this.health = 100;
-    this.level = 1;
-    this.attack = Character.prototype[type].attack;
-    this.defense = Character.prototype[type].defense
-  }
-  else {
-    throw new Error('Некорректные данные ввода')
-  }
-}
+import Char from "./classes/Char"
+import Character from "./classes/Char";
 
-Character.prototype = {
-  Bowman: {
-    attack: 25,
-    defense: 25,
-  },
-  Swordsman: {
-    attack: 40,
-    defense: 10,
-  },
-  Magician: {
-    attack: 10,
-    defense: 40,
-  },
-  Undead: {
-    attack: 25,
-    defense: 25,
-  },
-  Zombie: {
-    attack: 40,
-    defense: 10,
-  },
-  Daemon: {
-    attack: 10,
-    defense: 40,
-  },
-}
+const a = new Character("Chu", "Undead");
+console.log(a)
